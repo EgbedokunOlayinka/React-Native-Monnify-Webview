2f4feb6298dcc7b2e9327da156eff85f04f6c813
EgbedokunOlayinka <egbedokunolayinka@gmail.com>
2024-08-12 15:53:47 +0100

    update eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,9 @@
 module.exports = {
   root: true,
   extends: '@react-native',
-  rules: {'prettier/prettier': ['error', {endOfLine: 'auto'}]},
+  rules: {
+    'prettier/prettier': ['error', {endOfLine: 'auto'}],
+    'no-unused-vars': WARN,
+    'no-unused-expressions': IGNORE,
+  },
 };
